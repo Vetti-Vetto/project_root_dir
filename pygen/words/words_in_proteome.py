4c36afe3721303343fd9e860f07632928dcde516
Mountassira MATIN <mountassira.matin@etu.umontpellier.fr>
2023-11-19 17:42:47 +0100

    opens the .txt file correctly

--- pygen/words/words_in_proteome.py
+++ pygen/words/words_in_proteome.py
@@ -3,6 +3,6 @@ def read_file (file_name):
     with open(file_name, 'r') as file:
         content = file.read()
     return content
-file_name = 'english-common-words.txt'
+file_name = 'C:/Users/mount/Downloads/softdev project/project_root_dir/pygen/words/english-common-words.txt'
 content = read_file(file_name)
 print(content)
