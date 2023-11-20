23812bebef428201ecc1039db26f6f06c1a7a71e
Mountassira MATIN <mountassira.matin@etu.umontpellier.fr>
2023-11-20 12:11:06 +0100

    added human proteome file and started working on its code

--- pygen/words/words_in_proteome.py
+++ pygen/words/words_in_proteome.py
@@ -12,3 +12,28 @@ def count_words(file_name):
     return len(words)
 
 print("word count:", count_words('C:/Users/mount/Downloads/softdev project/project_root_dir/pygen/words/english-common-words.txt'))
+
+#Proteins
+def read_sequences(filename):
+    with open(filename, 'r') as file:
+        sequences = {}
+        current_sequence = ''
+        for line in file:
+            if line.startswith('>'):
+                if current_sequence:
+                    sequences[protein_id] = current_sequence
+                current_sequence = ''
+                protein_id = line.split('|')[1]
+            else:
+                current_sequence += line.strip()
+        if current_sequence:
+            sequences[protein_id] = current_sequence
+    return sequences
+
+def main():
+    sequences = read_sequences('human-proteome.fasta')
+    print('Number of sequences read:', len(sequences))
+    print('Sequence associated with the protein O95139:', sequences['O95139'])
+
+if __name__ == '__main__':
+    main()
